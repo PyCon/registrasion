3efec6b0edbb5c0b5d230c8452f7b80a6ed3ffc6
Ernest W Durbin III <ewdurbin@gmail.com>
2019-11-12 14:01:50 -0500

    ensure additional_data from ProductItem is addedto LineItems

--- registrasion/controllers/invoice.py
+++ registrasion/controllers/invoice.py
@@ -150,6 +150,7 @@ class InvoiceController(ForId, object):
                 quantity=item.quantity,
                 price=price,
                 product=product,
+                additional_data=item.additional_data,
             )
             line_items.append(line_item)
         for item in discount_items:
@@ -158,6 +159,7 @@ class InvoiceController(ForId, object):
                 quantity=item.quantity,
                 price=cls.resolve_discount_value(item) * -1,
                 product=item.product,
+                additional_data=item.additional_data,
             )
             line_items.append(line_item)
 
